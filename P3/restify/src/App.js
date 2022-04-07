dc15347d3c62ce0b1991ce5c4318acc1dd870c65
galegoer <eric.m.galego@gmail.com>
2022-04-07 11:11:39 -0400

    temp changes app routes

--- P3/restify/src/App.js
+++ P3/restify/src/App.js
@@ -4,7 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from "./components/Login/Login";
 import {useEffect} from "react";
-
+import {BrowserRouter, Route, Routes, Router, Switch} from "react-router-dom";
+// import {Route, Routes} from "react-router";
+import * as PropTypes from "prop-types";
+import Signup from "./components/Signup/Signup";
+// import Profile from "./components/Profile/Profile";
+// import Navbar from "./components/Navbar/Navbar";
+import BlogPostPage from './components/BlogPost/BlogPostPage';
 
 
 function App() {
@@ -13,26 +19,19 @@ function App() {
   }, []);
 
   return (
-
-      <Login/>
-
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact   element={<Login />} />
+        <Route path="/signup/"   element={<Signup/>} />
+        <Route path="/login/"   element={<Login />} />
+        {/* <Route path="/profile/" element={<Profile />} /> */}
+        {/* <Route path="/nav/"     element={<Navbar />} /> */}
+        <Route 
+          path="/blogs/:id"
+          element={<BlogPostPage />}
+        /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 

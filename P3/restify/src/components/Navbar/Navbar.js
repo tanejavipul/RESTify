e2dedb57a930879511a26cbc9b65ee70a4b7ff15
Vipul <tanejavipul@hotmail.com>
2022-04-16 15:44:49 -0400

    infinite scroll working on navbar

--- P3/restify/src/components/Navbar/Navbar.js
+++ P3/restify/src/components/Navbar/Navbar.js
@@ -5,13 +5,12 @@ import {Link} from "react-router-dom";
 import searchSVG from "../assets/Icons/search.svg"
 import homeSVG from "../assets/Icons/home.svg"
 import restSVG from "../assets/Icons/restaurant.svg"
-import notificationsSVG from "../assets/Icons/notifications.svg"
-import profileSVG from "../assets/Icons/profile.svg"
 import logoutSVG from "../assets/Icons/logout.svg"
 import ProfileDropDown from "./ProfileDropDown/ProfileDropDown";
+import NotificationsDropDown from "./NotificationsDropDown/NotificationsDropDown";
 
 
-const Navbar = () => {
+const Navbar = ({profileUpdate}) => {
 
 
 
@@ -19,15 +18,17 @@ const Navbar = () => {
         <>
             <nav className="navbar  navbar-background fixed-top">
                 <div className="container-fluid">
-                    <Link to="/profile" className="navbar-brand navbar-logo-color">
+                    <Link to="/home" className="navbar-brand navbar-logo-color">
                         <img src={restLogo} height="28" className="d-inline-block align-text-top"/> Restify
                     </Link>
                     <div className="ms-auto">
+                        {/*<span className="badge badge-light"></span>*/}
                         <Link to="/profile" className="btn  navbar-button-styling"> <img src={searchSVG}/>Search</Link>
                         <Link to="/profile" className="btn  navbar-button-styling"> <img src={homeSVG}/>Home</Link>
                         <Link to="/profile" className="btn  navbar-button-styling"> <img src={restSVG}/>Restaurant</Link>
+                        <NotificationsDropDown/>
                         {/* ADD NOTIFICATIONS*/}
-                        <ProfileDropDown/>
+                        <ProfileDropDown profileUpdate={profileUpdate}/>
                         <Link to="/profile" className="btn  navbar-button-styling"> <img src={logoutSVG}/>Logout</Link>
                     </div>
                 </div>
